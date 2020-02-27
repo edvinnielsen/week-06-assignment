16ac8dc28753451887a4d7df6177aedb4e158c09
Edvin Nielsen Johansson <enielsenjohansson@hawk.iit.edu>
2020-02-26 19:15:24 -0600

    Rename elemnts + change operators in functions
    All elements remamed to shorter and more consistent versions.
    Functions given correct operators.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,46 @@
-const addButton = document.querySelector("#add-solve");
-const subButton = document.querySelector("#subtract-solve");
-const multButton = document.querySelector("#multiply-solve");
-const divButton = document.querySelector("#divide-solve");
+const btnAdd = document.querySelector("#add-solve");
+const btnSub = document.querySelector("#subtract-solve");
+const btnMlt = document.querySelector("#multiply-solve");
+const btnDiv = document.querySelector("#divide-solve");
 
-const addNum1 = document.querySelector("#add-a");
-const addNum2 = document.querySelector("#add-b");
-const subNum1 = document.querySelector("#subtract-a");;
-const subNum2 = document.querySelector("#subtract-b");;
-const multNum1 = document.querySelector("#multiply-a");
-const multNum2 = document.querySelector("#multiply-b");
-const divNum1 = document.querySelector("#divide-a");
-const divNum2 = document.querySelector("#divide-b");
+const numAdd1 = document.querySelector("#add-a");
+const numAdd2 = document.querySelector("#add-b");
+const numSub1 = document.querySelector("#subtract-a");;
+const numSub2 = document.querySelector("#subtract-b");;
+const numMlt1 = document.querySelector("#multiply-a");
+const numMlt2 = document.querySelector("#multiply-b");
+const numDiv1 = document.querySelector("#divide-a");
+const numDiv2 = document.querySelector("#divide-b");
 
-const addResult = document.querySelector("#add-result");
-const subResult = document.querySelector("#subtract-result");
-const multResult = document.querySelector("#multiply-result");
-const divResult = document.querySelector("#divide-result");
+const resAdd = document.querySelector("#add-result");
+const resSub = document.querySelector("#subtract-result");
+const resMlt = document.querySelector("#multiply-result");
+const resDiv = document.querySelector("#divide-result");
 
 
 function add() {
-    const result = addNum1.valueAsNumber + addNum2.valueAsNumber;
-    addResult.value = result;
+    const result = numAdd1.valueAsNumber + numAdd2.valueAsNumber;
+    resAdd.value = result;
 }
 
 function sub() {
-    const result = subNum1.valueAsNumber + subNum2.valueAsNumber;
-    subResult.value = result;
+    const result = numSub1.valueAsNumber - numSub2.valueAsNumber;
+    resSub.value = result;
 }
 
-function mult() {
-    const result = multNum1.valueAsNumber + multNum2.valueAsNumber;
+function mlt() {
+    const result = numMlt1.valueAsNumber * numMlt2.valueAsNumber;
     console.log(result)
-    multResult.value = result;
+    resMlt.value = result;
 }
 
 function div() {
-    const result = divNum1.valueAsNumber + divNum2.valueAsNumber;
-    divResult.value = result;
+    const result = numDiv1.valueAsNumber / numDiv2.valueAsNumber;
+    resDiv.value = result;
 }
 
-addButton.addEventListener("click", add);
-subButton.addEventListener("click", sub);
-multButton.addEventListener("click", mult);
-divButton.addEventListener("click", div);
 
-
+btnAdd.addEventListener("click", add);
+btnSub.addEventListener("click", sub);
+btnMlt.addEventListener("click", mlt);
+btnDiv.addEventListener("click", div);
